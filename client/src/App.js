7b3829b670780ad5ff36dc1a722c639041343886
Aadil Glasswala <aglasswala@gmail.com>
2019-10-12 18:13:28 -0400

    added textfield

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Grid, TextField } from '@material-ui/core'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Grid
+                  container
+                  direction="column"
+                  justify="center"
+                  alignItems="center"
+                  style={{height: "100vh"}}
+                >
+                    <Grid item>
+                        <TextField
+                            placeholder="this is something great"
+                        />
+                    </Grid>
+                </Grid>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
