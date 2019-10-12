09302251c6b4bc2beb22da5898e09f287426e2e9
Aadil Glasswala <aglasswala@gmail.com>
2019-10-12 17:57:42 -0400

    image upload backend

---
+++ controllers/recognitionController.js
@@ -0,0 +1,5 @@
+module.exports = {
+	uploadImage: (req, res) => {
+		return res.status(200).send("image uploaded")
+	}
+}
--- routes.js
+++ routes.js
@@ -1,7 +1,11 @@
+const recognitionController = require('./controllers/recognitionController')
+
 module.exports = (router) => {
 	router.get('/', (req, res) => {
 		res.status(200).send({
 			"yes": "bitch"
 		})
 	})
+
+	router.post('/upload', recognitionController.uploadImage)
 }
