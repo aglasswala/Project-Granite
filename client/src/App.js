50b474084fe3433a30cbea384bff705bfdbb2620
Jeremiah Zagada <jeremiah.zagada16@gmail.com>
2019-10-12 18:25:29 -0400

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,6 @@
 import React, { Component } from 'react'
 import { Grid, TextField } from '@material-ui/core'
 
-<<<<<<< HEAD
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="URL-field"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-=======
 class App extends Component {
     render() {
         return (
@@ -42,7 +21,6 @@ class App extends Component {
             </div>
         )
     }
->>>>>>> 7b3829b670780ad5ff36dc1a722c639041343886
 }
 
 export default App
