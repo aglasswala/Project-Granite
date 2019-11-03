25fabe5560d3de83917e604c973da0f6415af06f
jtobin321 <jtobin321@gmail.com>
2019-11-03 01:05:08 -0400

    got things working

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+
+import NavBar from './components/NavBar'
 import Dashboard from './views/Dashboard.js'
+
 
 class App extends Component {
   render() {
     return (
-      <Dashboard />
+      <Fragment>
+        <NavBar />
+        <Dashboard />
+      </Fragment>
     )
   }
 }
