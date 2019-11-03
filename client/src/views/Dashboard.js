7a8db4822df6789b8265e96c93bfee675c5e6894
Matthew Wing <wingm1@montclair.edu>
2019-11-03 00:55:23 -0400

    changed dropdown in dashboard to full langauge names

--- client/src/views/Dashboard.js
+++ client/src/views/Dashboard.js
@@ -66,6 +66,7 @@ class Dashboard extends Component {
   }
 
   render() {
+    console.log(this.state.selectedLang);
     const { classes } = this.props
     return (
         <div>
@@ -105,7 +106,7 @@ class Dashboard extends Component {
                             onChange={this.changeLanguage}
                           >
                             {languages.map((lang, key) => {
-                              return <MenuItem key={key} value={lang}>{lang}</MenuItem>
+                              return <MenuItem key={key} value={lang.code}>{lang.language}</MenuItem>
                             })}
                           </Select>
                         </Grid>
