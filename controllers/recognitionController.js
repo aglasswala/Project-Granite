7d344be72d61bc7bc561af1d31c93ffd3b52675c
Aadil Glasswala <aglasswala@gmail.com>
2019-11-04 22:10:07 -0500

    eslint

--- controllers/recognitionController.js
+++ controllers/recognitionController.js
@@ -1,47 +1,48 @@
-const imageService = require('../services/imageService')
-const rekognitionService = require('../services/rekognitionService')
-const translateService = require('../services/translateService')
-const AWS = require('aws-sdk')
-AWS.config.update({region: process.env.REGION, accessKeyId: process.env.AWS_ACCESS_KEY_ID, secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY})
-var fs = require('fs');
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
+const AWS = require('aws-sdk');
+
 const s3 = new AWS.S3();
 
-const base64_encode = (file) => {
-    const bitmap = fs.readFileSync(file);
-	return new Buffer(bitmap).toString('base64');
-}
+const rekognitionService = require('../services/rekognitionService');
+const translateService = require('../services/translateService');
+
+AWS.config.update({
+  region: process.env.REGION,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
 
 module.exports = {
-	uploadImageS3: async (req,res) =>{
-		let params = {
-			Bucket: 'granite.project',
-			Key: '',
-			Body:''
-		}
-		let fileStream = fs.createReadStream(req.file.path)
-		fileStream.on('error', function(err){
-			console.log('File Error', err);
-		});
-		params.Body = fileStream;
-		params.Key = path.basename(req.file.originalname);
-		await new Promise((resolve,reject) => {
-			s3.upload(params, (err, data) => {
-				if (err) { 
-					reject(err);
-				}
-				if (data) {
-					resolve(data);
-				}
-			})
-		})
-			.catch(err => console.log(err))
-		
-		const stuff = await rekognitionService.getLabels(params)
-								.catch(err => res.status(400).send({err}))
-		const stuff2 = await translateService.translate(stuff, req.body.lang)
-								.catch(err => res.status(400).send({err}))
+  uploadImageS3: async (req, res) => {
+    const params = {
+      Bucket: 'granite.project',
+      Key: '',
+      Body: '',
+    };
+    const fileStream = fs.createReadStream(req.file.path);
+    fileStream.on('error', (err) => res.status(400).send({ err }));
 
-		return res.status(200).send(stuff2)
-	}
-}
+    params.Body = fileStream;
+    params.Key = path.basename(req.file.originalname);
+
+    await new Promise((resolve, reject) => {
+      s3.upload(params, (err, data) => {
+        if (err) {
+          reject(err);
+        }
+        if (data) {
+          resolve(data);
+        }
+      });
+    })
+      .catch((err) => res.status(400).send({ err }));
+
+    const stuff = await rekognitionService.getLabels(params)
+      .catch((err) => res.status(400).send({ err }));
+    const stuff2 = await translateService.translate(stuff, req.body.lang)
+      .catch((err) => res.status(400).send({ err }));
+
+    return res.status(200).send(stuff2);
+  },
+};
--- server.js
+++ server.js
@@ -1,26 +1,26 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-require("dotenv").config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
 
 const router = require('./routes');
 
 function startServer(server) {
-	const { PORT } = process.env
+  const { PORT } = process.env;
 
-	server.listen(PORT || 3001, () => {
-		console.log(`let's get it motherfucker, we live on ${PORT || 3001}`)
-	})
+  server.listen(PORT || 3001, () => {
+    console.log(`let's get it motherfucker, we live on ${PORT || 3001}`); // eslint-disable-line
+  });
 }
 
 async function init() {
-	const app = express()
+  const app = express();
 
-	app.use(bodyParser.json())
-	app.use(cors())
+  app.use(bodyParser.json());
+  app.use(cors());
 
-	router(app)
-	startServer(app)
+  router(app);
+  startServer(app);
 }
 
-init()
+init();
