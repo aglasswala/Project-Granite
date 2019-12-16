9475cd58c111681cee07406af53727b3c90c28ba
Aadil Glasswala <aglasswala@gmail.com>
2019-12-15 21:22:23 -0500

    hooks

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,13 @@ import React, { Component, Fragment } from 'react'
 import NavBar from './components/NavBar'
 import Dashboard from './views/Dashboard.js'
 
-import './styles/app.css'
-
-class App extends Component {
-
-	render() {
-	    return (
-	    	<Fragment>
-		    	<NavBar />
-		    	<Dashboard />
-		    </Fragment>
-	    )
-	}
+const App = () => {
+	return (
+    	<Fragment>
+	    	<NavBar />
+	    	<Dashboard />
+	    </Fragment>
+	)
 }
 
-export default (App)
+export default App
